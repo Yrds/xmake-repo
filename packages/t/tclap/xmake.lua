20fcab5dba9425ba364ffdac2026a6a4d0b60af9
Yuri dos Santos <yrds96@protonmail.com>
2025-12-09 01:38:49 -0300

    Add package tclap 1.4

--- packages/t/tclap/xmake.lua
+++ packages/t/tclap/xmake.lua
@@ -8,6 +8,7 @@ package("tclap")
              "https://git.code.sf.net/p/tclap/code.git")
 
     add_versions("v1.2.5", "7e87d13734076fa4f626f6144ce9a02717198b3f054341a6886e2107b048b235")
+    add_versions("v1.4.0-rc1", "799a8b1f99818e39fee19d0601030770af1221e1")
 
     on_install(function (package)
         os.cp("include/tclap", package:installdir("include"))

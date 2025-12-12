b4dd2837b7d9c203cfdf864df46939a75d70cc5f
Yuri dos Santos <yrds96@protonmail.com>
2025-12-12 15:43:34 -0300

    add tigr

---
+++ packages/t/tigr/xmake.lua
@@ -0,0 +1,13 @@
+package("tigr")
+    set_kind("library")
+    set_homepage("https://github.com/erkkah/tigr")
+    set_description("TIGR - the TIny GRaphics library for Windows, macOS, Linux, iOS and Android.")
+    set_license("UNLICENSE")
+
+    add_urls("https://github.com/erkkah/tigr/archive/refs/tags/($version).tar.gz")
+
+    add_versions("v3.2.2", "")
+
+    on_install(function (package)
+        os.cp("tigr.h", package:installdir("include/tigr.h"))
+    end)

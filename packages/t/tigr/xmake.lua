76bdcde3a049931d6f41e894cefc0b92dea62d77
Yuri dos Santos <yrds96@protonmail.com>
2025-12-12 15:51:54 -0300

    Fix hash

--- packages/t/tigr/xmake.lua
+++ packages/t/tigr/xmake.lua
@@ -6,7 +6,7 @@ package("tigr")
 
     add_urls("https://github.com/erkkah/tigr/archive/refs/tags/$(version).tar.gz")
 
-    add_versions("v3.2.2", "7a76039b")
+    add_versions("v3.2.2", "7a76039bf2afa0637b6e7ee31f8d8050ca192a5104bc53e6d14927cc50f64c12")
 
     on_install(function (package)
         os.cp("tigr.h", package:installdir("include/tigr.h"))

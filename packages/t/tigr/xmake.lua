d43788571105a0d1f7e22a132e871c9199996b11
Yuri dos Santos <yrds96@protonmail.com>
2025-12-12 19:38:12 -0300

    add test to tigr

--- packages/t/tigr/xmake.lua
+++ packages/t/tigr/xmake.lua
@@ -27,3 +27,18 @@ package("tigr")
         ]])
         import("package.tools.xmake").install(package)
     end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include "tigr.h"
+            void test() {
+                Tigr *screen = tigrWindow(320, 240, "Hello", 0);
+                while (!tigrClosed(screen)) {
+                    tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
+                    tigrPrint(screen, tfont, 120, 110, tigrRGB(0xff, 0xff, 0xff), "Hello, world.");
+                    tigrUpdate(screen);
+                }
+                tigrFree(screen);
+            }
+        ]]}))
+    end)
